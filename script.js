3dee255e628512776691b1ea970337d699860f3f
artem20124708 <artem.tarasenko2012@gmail.com>
2023-04-04 15:47:05 +0300

    js 2

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 // Intro scrollers
 
-function scroller(focusedScroller, scrollElem) {
+function introScroller(focusedScroller, scrollElem) {
     document.querySelector('.scroller_active').classList.remove("scroller_active")
     focusedScroller.classList.add("scroller_active")
     scrollElem.scrollIntoView({behavior: "smooth"})
 }
 
 document.querySelector('#scrollToIntro').addEventListener('click', function() {
-    scroller(this, document.querySelector('.intro'))
+    introScroller(this, document.querySelector('.intro'))
 })
 
 document.querySelector('#scrollToWwedo').addEventListener('click', function() {
-    scroller(this, document.querySelector('.wwedo__title'))
+    introScroller(this, document.querySelector('.wwedo__title'))
 })
 document.querySelector('#scrollToBestwork').addEventListener('click', function() {
-    scroller(this, document.querySelector('.bestwork__title'))
+    introScroller(this, document.querySelector('.bestwork__title'))
 })
 document.querySelector('#scrollToBestteam').addEventListener('click', function() {
-    scroller(this,document.querySelector('.bestteam'))
+    introScroller(this,document.querySelector('.bestteam'))
 })
 document.querySelector('#scrollToClients').addEventListener('click', function() {
-    scroller(this, document.querySelector('#clientsSay'))
+    introScroller(this, document.querySelector('#clientsSay'))
+})
+
+// Clients
+
+function clientsScroller(focusedScroller, scrollElem) {
+    document.querySelector('.clients__scroller_active').classList.remove("clients__scroller_active")
+    focusedScroller.classList.add("clients__scroller_active")
+    document.querySelector('.card_active').classList.remove("card_active")
+    scrollElem.classList.add("card_active")
+}
+
+document.querySelectorAll('.firstClient').addEventListener('click', function(){
+    clientsScroller(this, document.querySelector('#clientOne'))
+})
+document.querySelectorAll('.secondClient').addEventListener('click', function(){
+    clientsScroller(this, document.querySelector('#clientTwo'))
+})
+document.querySelectorAll('.thirdClient').addEventListener('click', function(){
+    clientsScroller(this, document.querySelector('#clientThree'))
+})
+document.querySelectorAll('.fourthClient').addEventListener('click', function(){
+    clientsScroller(this, document.querySelector('#clientFour'))
 })
