c3e0e49dfc5838aed5db3051787b8ad935b2c741
artem20124708 <artem.tarasenko2012@gmail.com>
2023-04-04 14:18:56 +0300

    Js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // Intro scrollers
 
-function scroller(scrollElem) {
+function scroller(focusedScroller, scrollElem) {
+    document.querySelector('.scroller_active').classList.remove("scroller_active")
+    focusedScroller.classList.add("scroller_active")
     scrollElem.scrollIntoView({behavior: "smooth"})
 }
 
+document.querySelector('#scrollToIntro').addEventListener('click', function() {
+    scroller(this, document.querySelector('.intro'))
+})
+
 document.querySelector('#scrollToWwedo').addEventListener('click', function() {
-    scroller(document.querySelector('.wwedo__title'))
+    scroller(this, document.querySelector('.wwedo__title'))
 })
 document.querySelector('#scrollToBestwork').addEventListener('click', function() {
-    scroller(document.querySelector('.bestwork__title'))
+    scroller(this, document.querySelector('.bestwork__title'))
 })
 document.querySelector('#scrollToBestteam').addEventListener('click', function() {
-    scroller(document.querySelector('.bestteam'))
+    scroller(this,document.querySelector('.bestteam'))
 })
 document.querySelector('#scrollToClients').addEventListener('click', function() {
-    scroller(document.querySelector('#clientsSay'))
+    scroller(this, document.querySelector('#clientsSay'))
 })
